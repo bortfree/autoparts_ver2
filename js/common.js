5ac7efed795a6f5264dc4e3781dcafaf618b2e95
Evgeny Bortnikov <ebortnikov@mail.ru>
2016-02-22 11:52:10 +0300

    done

---
+++ js/common.js
@@ -0,0 +1,68 @@
+$(function() {
+
+	$(".toggle-mnu").click(function(){
+          $(this).toggleClass("on");
+          $(".menu").slideToggle();
+        });
+
+	$(".popup").magnificPopup();
+
+	$(".menu ul li a, .button, .prosm_cat").mPageScroll2id();
+
+	
+
+	
+	$(".equal_sect4").equalHeights();
+
+	
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$.magnificPopup.open({
+        items: {
+          src: '.done'
+        },
+        type: 'inline'
+      });
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
